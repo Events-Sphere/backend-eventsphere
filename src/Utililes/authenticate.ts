c88ca12ec93355afc72b9af4c04984452600b17b
SURIYA-8273 <suriyasivakumar60@gmail.com>
2024-12-31 17:03:13 +0530

    OTHER : pulling existing code

--- src/Utililes/authenticate.ts
+++ src/Utililes/authenticate.ts
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken";
 export class Authenticate {
   static async isAuthenticated(req: Request, res: Response) {
     try {
+      
     } catch (error) {
       return ApiResponseHandler.error(res, "Internal server error", 501);
     }
